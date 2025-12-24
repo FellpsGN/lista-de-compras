2d4cf0a139188dff21fcc93dce257851f2e5f63e
FellpsGN <fellps.program06@gmail.com>
2025-12-24 19:04:00 -0300

    :tada: Initial Commit

---
+++ scripts.js
@@ -0,0 +1,50 @@
+const input = document.getElementById("item-input")
+const addButton = document.getElementById("add-button")
+const shoppingList = document.getElementById("shopping-list")
+const alertRemoved = document.querySelector(".alert-removed")
+
+addButton.addEventListener("click", () => {
+    if(!(input instanceof HTMLInputElement)) return
+
+    const itemName = input.value.trim()
+
+    if(itemName === "") return
+    
+    const listItem = document.createElement("li")
+    listItem.classList.add("item")
+
+    const itemContainer = document.createElement("div")
+    itemContainer.classList.add("conteiner-item")
+    
+    const checkbox = document.createElement("input")
+    checkbox.type = "checkbox"
+    checkbox.classList.add("check")
+    itemContainer.appendChild(checkbox)
+    
+    const span = document.createElement("span")
+    span.textContent = itemName
+    itemContainer.appendChild(span)
+    console.log(itemContainer)
+    listItem.appendChild(itemContainer)
+    
+    const deleteIcon = document.createElement("i")
+    deleteIcon.classList.add("hgi-stroke", "hgi-delete-01", "lixeira")
+    deleteIcon.setAttribute("aria-hidden", "true")
+    listItem.appendChild(deleteIcon)
+    shoppingList.appendChild(listItem)
+    
+    deleteIcon.addEventListener("click", () => {
+        shoppingList.removeChild(listItem)
+        alertRemoved.classList.toggle("hidden")
+        setTimeout(() => {
+            alertRemoved.classList.add("hidden")
+        }, 3000)
+    }
+    )
+
+    input.value = ""
+})
+
+alertRemoved.querySelector(".close").addEventListener("click", () => {
+    alertRemoved.classList.toggle("hidden")
+})
